10ce3515d183cd7a97b3154c8bcaf739f9880dcc
ysur67 <rus.muratow@gmail.com>
2022-08-03 23:59:12 +0300

    move total price block to separate file

--- src/pages/index/page.tsx
+++ src/pages/index/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useOrdersQuery } from "../../api/orders/GetAllOrders";
-import { Order } from "../../models/order";
 import OrdersChart from "./components/OrdersChart";
 import OrdersTable from "./components/OrdersTable";
+import TotalOrdersPriceBlock from "./components/TotalOrderPriceBlock";
 
 const IndexPage: React.FC = () => {
   const { isLoading, data, error: hasErrors } = useOrdersQuery();
@@ -17,10 +17,10 @@ const IndexPage: React.FC = () => {
 
   return (
     <div className="flex flex-row w-full justify-between p-10">
-      <OrdersChart orders={data!} />
+      <OrdersChart orders={data} />
       <div className="w-full p-2 flex flex-col items-center">
-        <TotalOrdersPriceBlock orders={data!} />
-        <OrdersTable orders={data!} />
+        <TotalOrdersPriceBlock orders={data} />
+        <OrdersTable orders={data} />
       </div>
     </div>
   );
@@ -34,25 +34,6 @@ export const IndexPageShimmer: React.FC = () => {
         <Skeleton height={100} width={350} className="mb-4" />
         <Skeleton height={600} />
       </div>
-    </div>
-  );
-};
-
-interface TotalOrderPriceBlockProps {
-  orders: Array<Order>;
-}
-
-export const TotalOrdersPriceBlock: React.FC<TotalOrderPriceBlockProps> = ({
-  orders,
-}) => {
-  const totalPrice = orders.reduce<number>(
-    (prev, current) => prev + current.costDollars,
-    0
-  );
-  return (
-    <div className="flex flex-col w-80 border-4 border-black">
-      <span className="bg-blue-800 text-white text-2xl">Total</span>
-      <span className="text-6xl">{totalPrice}</span>
     </div>
   );
 };
